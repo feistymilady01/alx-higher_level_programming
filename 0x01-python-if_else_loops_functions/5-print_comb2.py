c034a19379cbb875f354edbea6624971f73a132c
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-11-29 22:58:22 +0100

    range of numers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,1 +1,6 @@
-5-print_comb2.py
+#!/usr/bin/python3
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
