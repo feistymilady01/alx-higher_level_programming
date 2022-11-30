5f50d5c515b4431965cf453e13022b498946340b
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-11-30 20:26:56 +0100

    func uppercas

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,7 @@
-8-uppercase.py
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if (ord(char) >= 97) and (ord(char) <= 122):
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end='')
+    print()
