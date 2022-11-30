9cfcd922984ec1ba10c9020ff090fe1e741797be
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-11-30 20:16:24 +0100

    removing char

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,2 +1,7 @@
-101-remove_char_at.py
-
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    if n >= 0:
+        str_cpy = str[:n] + str[n + 1:]
+        return (str_cpy)
+    else:
+        return (str)
