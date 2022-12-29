2c77e15d20290de2969b5a83f810d75748dfd8a8
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-12-29 16:08:41 +0100

    test_indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,23 @@
-kwfn
+#!/usr/bin/python3
+"""
+Module 5-text_indentation
+"Tokenize" a string with specified delimiters
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after ".,?:"
+    Args:
+        text: (str)
+    Raises:
+        TypeError: text must be a string
+    """
+
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
