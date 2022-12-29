2d667be4680cbdcccca9adffeb1da2e99394f511
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-12-29 16:02:52 +0100

    test sum

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,13 +16,14 @@ def add_integer(a, b=98):
     Returns:
         int: sum of a and b
     """
-if not isinstance(a, int) and not isinstance(a, float):
-  raise TypeError("a must be an integer")
-if not isinstance(b, int) and not isinstance(b, float):
-  raise TypeError("b must be an integer")
-if type(a) is float:
-  a = int(a)
-if type(b) is float:
-  b = int(b)
-  
- return a + b
+
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+
+    return a + 
