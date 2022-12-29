bdd0af355a0a39fb0a36cc1cc3ac3181197eb85f
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-12-29 16:04:35 +0100

    test_matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -18,18 +18,18 @@ def matrix_divided(matrix, div):
     Returns a new matrix.
     """
 
-if not isinstance(matrix, list):
-  raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-for row in matrix:
-  for rows in matrix:
-    if len(row) != len(rows):
-      raise TypeError("Each row of the matrix must have the same size")
-if not isinstance(div, int) and not isinstance(div, float):
-  raise TypeError("div must be a number")
-if div == 0:
-  raise ZeroDivisionError("division by zero")
-  
-new_matrix = []
-for row in range(len(matrix)):
-  new_matrix.append(list(map(lambda y: round(y / div, 2), matrix[row])))
-return new_matrix
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for row in matrix:
+        for rows in matrix:
+            if len(row) != len(rows):
+                raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = []
+    for row in range(len(matrix)):
+        new_matrix.append(list(map(lambda y: round(y / div, 2), matrix[row])))
+    return new_matrix
