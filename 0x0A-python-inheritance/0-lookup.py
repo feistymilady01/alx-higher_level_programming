5333d40a0eeec2e597747ea7d3881faf2e018831
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-01-09 23:00:39 +0100

    update

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,17 @@
-msms
+#!/usr/bin/python3
+""" 0-lookup Module
+Gets a list of available attributes and methods
+of an object
+"""
+
+
+def lookup(obj):
+    """ Returns the list of available attributes and methods
+    of an object
+    Args:
+        obj: An instance of a class
+    Returns:
+        A list of availabe methids and attribute
+    """
+
+    return (dir(obj))
