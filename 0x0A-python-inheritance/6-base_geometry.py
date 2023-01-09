5d0e8bd2c307cb695359ceae97465cabe56c1411
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-01-09 23:07:28 +0100

    update

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,14 @@
-msm
+#!/usr/bin/python3
+""" 6-base-geometry Module
+Houses a class BaseGeometry that
+raises an Exception
+"""
+
+
+class BaseGeometry:
+    """ Defines the class BaseGeometry"""
+
+    def area(self):
+        """ Raises an Exception """
+
+        raise Exception('area() is not implemented')
