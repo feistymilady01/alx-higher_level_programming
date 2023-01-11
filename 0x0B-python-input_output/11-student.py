55a485bed199098edeacfb1686d4b925c4c7e6be
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-01-11 17:53:06 +0100

    update

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,42 +1,48 @@
 #!/usr/bin/python3
-import os
-import sys
-
-Student = __import__('11-student').Student
-read_file = __import__('0-read_file').read_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-path = sys.argv[1]
-
-if os.path.exists(path):
-    os.remove(path)
-
-student_1 = Student("John", "Doe", 23)
-j_student_1 = student_1.to_json()
-print("Initial student:")
-print(student_1)
-print(type(student_1))
-print(type(j_student_1))
-print("{} {} {}".format(student_1.first_name, student_1.last_name, student_1.age))
+""" 11-student Module
+Houses a class Student
+"""
 
 
-save_to_json_file(j_student_1, path)
-read_file(path)
-print("\nSaved to disk")
+class Student:
+    """ Defines the class Student """
 
+    def __init__(self, first_name, last_name, age):
+        """ Initializes an instance of Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-print("Fake student:")
-new_student_1 = Student("Fake", "Fake", 89)
-print(new_student_1)
-print(type(new_student_1))
-print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation of
+        a Student instance
+        """
 
+        if type(attrs) is list:
+            for word in attrs:
+                if type(word) is not str:
+                    return self.__dict__
 
-print("Load dictionary from file:")
-new_j_student_1 = load_from_json_file(path)
+            new_dict = {}
 
-new_student_1.reload_from_json(j_student_1)
-print(new_student_1)
-print(type(new_student_1))
-print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
+            # Moving through he list in attrs
+            for element in range(len(attrs)):
+                for key in self.__dict__:
+                    # if any elemnet in list attrs is the same as a key
+                    # in self.__dict__
+                    if attrs[element] == key:
+                        # copy that element into a new dict
+                        new_dict[key] = self.__dict__[key]
+            return new_dict
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes o fthe Student instance
+        Args:
+            json: A dictonary
+        """
+
+        for atr in json:
+            self.__dict__[atr] = json[atr]
