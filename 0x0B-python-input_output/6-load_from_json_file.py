790ef1b88c7d28ce4846b50ea36bb5f4f3d81b64
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-01-11 18:00:15 +0100

    load json

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,15 @@
-h
+#!/usr/bin/python3
+""" 6-load_from_json_file Module
+Defines a function load_from_json_file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """ Creates an Onject from a "JSON" file
+    Args:
+        filename: JSON file
+    """
+
+    with open(filename) as f:
+        return json.load(f)
