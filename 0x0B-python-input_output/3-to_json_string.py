4cf21784a195851e53baca08b8b9e9a755a4e63f
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-01-11 17:56:59 +0100

    json string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,15 @@
-w
+#!/usr/bin/python3
+""" 3-to_json_string Module
+Defines a function t0_json_string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """ Retuns the JSON representation of an object(string)
+    Args:
+        my_obj: A python object
+    Returns: a JSON representation of my_obj
+    """
+
+    return json.dumps(my_obj)
