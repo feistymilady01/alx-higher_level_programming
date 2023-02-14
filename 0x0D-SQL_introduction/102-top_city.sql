f6489f74c148fc645b9452595ed20d074db851ff
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-02-14 19:46:47 +0100

    display db

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,1 +1,8 @@
-i
+-- Displays the top 3 cities temperature during July and August
+-- ordered by temperature in descending order
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
