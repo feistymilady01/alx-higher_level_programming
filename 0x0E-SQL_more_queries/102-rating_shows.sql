e6667c7cc5d1db61f29a03bb10ec74266d9ed3a0
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-02-16 17:58:02 +0100

    rating db

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,1 +1,6 @@
-iiiii
+-- List all shows by their ratings
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
