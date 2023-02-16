1ec8ab1e868bbbaab05d711e21ea692eab40d3e6
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-02-16 17:52:29 +0100

    create user db

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,1 +1,7 @@
-ii
+-- Creates user_0d_1 with all privileges on MySQL server with password
+-- user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+	ON *.*
+	TO 'user_0d_1'@'localhost';
