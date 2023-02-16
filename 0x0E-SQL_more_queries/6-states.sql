f25f9d18194e3f9022c797fc62f6253d3875a56d
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-02-16 18:16:20 +0100

    state db

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,1 +1,6 @@
-iiii
+-- Creates the database hbtn_0d_usa and the table states in hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+	);
