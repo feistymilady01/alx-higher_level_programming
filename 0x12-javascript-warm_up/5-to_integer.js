e21d0881c11003dd5fec21c3da1741dcda9d76b7
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-03-14 00:51:55 +0100

    update

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,2 @@
 #!/usr/bin/node
-const x = process.argv[2];
-
-if (!parseInt(x)) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < x; i++) {
-    console.log('C is fun');
-  }
-}
+console.log(parseInt(process.argv[2]) ? `My number: ${parseInt(process.argv[2])}` : 'Not a number');
