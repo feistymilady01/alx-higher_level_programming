e9adaed51099e41e8d4965bac53cbb3abf8f88f0
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-03-14 00:50:01 +0100

    loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i = 0;
-
-for (i; i < 3; i++) {
-  console.log(array[i]);
-}
+myVar.forEach((element) => {
+  console.log(element);
+});
