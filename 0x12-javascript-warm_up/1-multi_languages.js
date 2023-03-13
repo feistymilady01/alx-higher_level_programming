95065e7ac1a68f62df381f3141170dc88dd40314
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-03-14 00:08:52 +0100

    update

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+const newLine = '\n';
+console.log(firstLine + newLine + secondLine + newLine + thirdLine);
