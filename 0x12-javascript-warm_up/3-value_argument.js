322e27fe5b02141d69957566bc10d2e6e833bfd7
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-03-14 00:47:57 +0100

    value

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-
-const args = process.argv;
-
-if (args[2] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(args[2]);
-}
+console.log(process.argv[2] ? process.argv[2] : 'No argument');
