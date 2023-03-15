bdefb516f432adcfc91bde439fef85b49a428d63
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-03-16 00:32:58 +0100

    updated

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
+const dict = require('./101-data').dict;
 
-const occurences = {};
-
-for (const userId in dict) { // looping through the object key
-  const count = dict[userId]; // assigns the value to a variable
-  if (count in occurences) { // if key exists
-    occurences[count].push(userId); // access the object key, add to the value list
-  } else {
-    occurences[count] = [userId]; // if no key, add the key to the object
+const totalist = Object.entries(dict);
+const vals = Object.values(dict);
+const valsUniq = [...new Set(vals)];
+const newDict = {};
+for (const j in valsUniq) {
+  const list = [];
+  for (const k in totalist) {
+    if (totalist[k][1] === valsUniq[j]) {
+      list.unshift(totalist[k][0]);
+    }
   }
+  newDict[valsUniq[j]] = list;
 }
-console.log(occurences);
+console.log(newDict);
