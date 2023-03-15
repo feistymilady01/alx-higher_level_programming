24e6ca271ff97fe816cdffd31653c02f84df8864
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-03-16 00:31:24 +0100

    concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-// read the contents of fileA and fileB
-const fileA = fs.readFileSync(process.argv[2], 'utf8');
-const fileB = fs.readFileSync(process.argv[3], 'utf8');
-const result = fileA + fileB;
 
-fs.writeFileSync(process.argv[4], result);
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
